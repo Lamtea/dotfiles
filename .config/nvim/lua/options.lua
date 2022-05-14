8b4d524ac3c26ea9ec81819894f51a60e4996a7d
lamt <caramelamcha@gmail.com>
2022-05-15 04:15:19 +0900

    update nvim.

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,12 +1,27 @@
 local o = vim.o
+local bo = vim.bo
+local wo = vim.wo
 
 -- Options.
-o.number=true
-o.autoindent=true
-o.expandtab=true
-o.tabstop=4
-o.shiftwidth=4
-o.backspace='indent,eol,start'
-o.wildmenu=true
-o.wildmode='list:longest,full'
-o.clipboard='unnamed,unnamedplus'
+o.helplang = 'ja,en'
+o.ignorecase = true
+o.smartcase = true
+o.splitright = true
+o.termguicolors = true
+o.hidden = true
+o.updatetime = 300
+o.termguicolors = true
+o.backspace = 'indent,eol,start'
+o.wildmenu = true
+o.wildmode = 'list:longest,full'
+o.clipboard = 'unnamed,unnamedplus'
+bo.expandtab = true
+bo.autoindent = true
+bo.smartindent = true
+bo.tabstop = 4
+bo.shiftwidth = 4
+bo.autoread = true
+wo.number = true
+wo.relativenumber = true
+wo.signcolumn = 'yes'
+wo.cursorline = true
