2c1068cb937d760df2c806ed546e43a6aee141a3
lamt <caramelamcha@gmail.com>
2022-05-15 03:55:58 +0900

    update nvim.

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -1,4 +1,11 @@
 -- Commands.
 vim.cmd('syntax on')
 vim.cmd('filetype plugin indent on')
-vim.cmd('colorscheme murphy')
+-- vim.cmd('colorscheme murphy')
+
+-- Auto commands.
+vim.cmd('augroup fcitx')
+vim.cmd('autocmd!')
+vim.cmd([[autocmd InsertLeave * :call system('fcitx5-remote -c')]])
+vim.cmd([[autocmd CmdlineLeave * :call system('fcitx5-remote -c')]])
+vim.cmd('augroup END')
