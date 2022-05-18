e6a5b83f2c0728ee8603caf3ff200658a4df3f8e
lamt <caramelamcha@gmail.com>
2022-05-18 13:02:27 +0900

    update nvim.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -90,7 +90,7 @@ require'packer'.startup(function()
                 fuzzy = true,
                 override_generic_sorter = true,
                 override_file_sorter = true,
-                case_mode = "smart_case"
+                case_mode = 'smart_case'
             }
         }
     }
