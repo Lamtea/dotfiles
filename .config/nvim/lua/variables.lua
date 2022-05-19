bcd5564fc286e2d991cf4ba439d7231e4aa74ad7
lamt <caramelamcha@gmail.com>
2022-05-20 06:26:31 +0900

    update nvim.

--- .config/nvim/lua/variables.lua
+++ .config/nvim/lua/variables.lua
@@ -3,6 +3,7 @@ local api = vim.api                                 -- neovim api
 -- 変数設定
 local vars = {
     python3_host_prog = '/usr/bin/python3',         -- neovimのpythonサポート
+    loaded_perl_provider = 0,                       -- neovimのperlサポート無効
     Illuminate_delay = 500,                         -- vim-illuminateの単語をハイライトするまでの時間
     Illuminate_highlightUnderCursor = 0,            -- vim-illuminateのカーソル位置の単語はハイライトしない
     neo_tree_remove_legacy_commands = 1             -- neo-treeのレガシーコマンドは使用しない
