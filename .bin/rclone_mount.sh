c58c20bc6695536029a80ef61d9955e53be3568f
lamt <caramelamcha@gmail.com>
2022-05-21 20:36:14 +0900

    update script.

--- .bin/rclone_mount.sh
+++ .bin/rclone_mount.sh
@@ -1,22 +1,22 @@
 #! /bin/bash
 
 clouds=(
-    dropbox
-    gdrive
-    onedrive
-    pcloud
-    mega
+	dropbox
+	gdrive
+	onedrive
+	pcloud
+	mega
 )
 
 for cloud in "${clouds[@]}"; do
-  mountpath="${HOME}/clouds/${cloud}"
-  if [[ ! -e "${mountpath}" ]]; then
-    mkdir -p "${mountpath}"
-  fi
+	mountpath="${HOME}/clouds/${cloud}"
+	if [[ ! -e "${mountpath}" ]]; then
+		mkdir -p "${mountpath}"
+	fi
 
-  if [ "${cloud}" = "dropbox" ]; then
-    rclone mount --vfs-cache-mode writes "${cloud}:Data" "${mountpath}" &
-  else
-    rclone mount --vfs-cache-mode writes "${cloud}:/" "${mountpath}" &
-  fi
+	if [ "${cloud}" = "dropbox" ]; then
+		rclone mount --vfs-cache-mode writes "${cloud}:Data" "${mountpath}" &
+	else
+		rclone mount --vfs-cache-mode writes "${cloud}:/" "${mountpath}" &
+	fi
 done
