bbc49572e4d825e4320463244c6b5f5733c67190
lamt <caramelamcha@gmail.com>
2022-05-22 15:54:34 +0900

    update neovim.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -191,6 +191,7 @@ require("packer").startup(function(use)
 	null_ls.setup({
 		sources = {
 			-- code action
+			null_ls.builtins.code_actions.gitsigns, -- gitsignsをコードアクションで実行できる
 			null_ls.builtins.code_actions.eslint, -- for javascript/typescript
 			null_ls.builtins.code_actions.shellcheck, -- for bash
 			-- diagnostics
@@ -1066,7 +1067,7 @@ require("packer").startup(function(use)
 		end,
 	})
 	use({
-		"sindrets/diffview.nvim", -- git差分(キーバインドは標準 see: github)
+		"sindrets/diffview.nvim", -- git diff(キーバインドは標準 see: github)
 		requires = {
 			"nvim-lua/plenary.nvim",
 		},
@@ -1083,7 +1084,64 @@ require("packer").startup(function(use)
 		"<Cmd>DiffviewFileHistory<CR>",
 		{ noremap = true, silent = true }
 	)
-	vim.api.nvim_set_keymap("n", "gq", "<Cmd>tabclose<CR>", { noremap = true, silent = true })
+
+	use({
+		"lewis6991/gitsigns.nvim", -- gitの状態をカラムにサイン表示
+		config = function()
+			require("gitsigns").setup({
+				on_attach = function(bufnr)
+					local gs = package.loaded.gitsigns
+
+					local function map(mode, l, r, opts)
+						opts = opts or {}
+						opts.buffer = bufnr
+						vim.keymap.set(mode, l, r, opts)
+					end
+
+					-- Navigation
+					map("n", "]h", function()
+						if vim.wo.diff then
+							return "]h"
+						end
+						vim.schedule(function()
+							gs.next_hunk()
+						end)
+						return "<Ignore>"
+					end, { expr = true })
+
+					map("n", "[h", function()
+						if vim.wo.diff then
+							return "[h"
+						end
+						vim.schedule(function()
+							gs.prev_hunk()
+						end)
+						return "<Ignore>"
+					end, { expr = true })
+
+					-- Actions
+					map({ "n", "v" }, "<leader><leader>gs", ":Gitsigns stage_hunk<CR>")
+					map({ "n", "v" }, "<leader><leader>gr", ":Gitsigns reset_hunk<CR>")
+					map("n", "<leader><leader>gS", gs.stage_buffer)
+					map("n", "<leader><leader>gu", gs.undo_stage_hunk)
+					map("n", "<leader><leader>gR", gs.reset_buffer)
+					map("n", "<leader><leader>gp", gs.preview_hunk)
+					map("n", "<leader><leader>gb", function()
+						gs.blame_line({ full = true })
+					end)
+					map("n", "<leader><leader>gB", gs.toggle_current_line_blame)
+					map("n", "<leader><leader>gt", gs.diffthis)
+					map("n", "<leader><leader>gT", function()
+						gs.diffthis("~")
+					end)
+					map("n", "<leader><leader>gD", gs.toggle_deleted)
+
+					-- Text object
+					map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+				end,
+			})
+		end,
+	})
 
 	-- コマンド
 	use("mileszs/ack.vim") -- :Ack
