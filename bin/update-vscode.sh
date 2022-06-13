b0a480c81d8e3c896f834a878fef4f86a4523468
lamt <caramelamcha@gmail.com>
2022-06-13 11:01:05 +0900

    add update-vscode-php-debug.sh.

--- bin/update-vscode.sh
+++ bin/update-vscode.sh
@@ -3,5 +3,6 @@
 update-vscode-node-debug2.sh &&
 	update-vscode-chrome-debug.sh &&
 	update-vscode-firefox-debug.sh &&
+	update-vscode-php-debug.sh &&
 	update-java-debug.sh &&
 	update-vscode-java-test.sh
--- install.sh
+++ install.sh
@@ -75,6 +75,7 @@ dotfiles_or_dirs=(
 	bin/update-vscode-node-debug2.sh
 	bin/update-vscode-chrome-debug.sh
 	bin/update-vscode-firefox-debug.sh
+	bin/update-vscode-php-debug.sh
 	bin/update-java-debug.sh
 	bin/update-vscode-java-test.sh
 	bin/update-lsp.sh
