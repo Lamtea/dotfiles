2d61027729696f00d639ec8e06df1a8350420d85
lamt <caramelamcha@gmail.com>
2022-10-15 23:38:17 +0900

    fix(neovim): change update_capability to default_capability in lsp.lua.

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -93,12 +93,12 @@ m.on_attach = function(_, bufnr)
 end
 
 m.get_capabilities = function()
-    return require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    return require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 end
 
 local function get_clang_capabilities()
     -- NOTE: Warning will be issued if encoding is not specified.
-    local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
     capabilities.offsetEncoding = { "utf-16" }
     return capabilities
 end
