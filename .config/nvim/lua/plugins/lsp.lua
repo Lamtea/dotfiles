6a506e2b4b9b8d87934b106899a8cbb6f96335c1
lamt <caramelamcha@gmail.com>
2023-11-14 00:56:04 +0900

    feat(neovim): add protobuf lsp.

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -113,7 +113,10 @@ local function get_clang_capabilities()
 end
 
 local function setup_lsp_clangd(serverconfig, on_attach)
+    local lspconfig = require("lspconfig")
     serverconfig.setup({
+        -- Exclude ".proto"
+        filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
         on_attach = on_attach,
         capabilities = get_clang_capabilities(),
     })
