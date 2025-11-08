1ec5fdd5ff86b9f71159b603bd8f816e9be7b00a
lamt <caramelamcha@gmail.com>
2025-11-08 13:32:05 +0900

    feat: update packages.

--- bin/update-php-composer.sh
+++ bin/update-php-composer.sh
@@ -4,13 +4,13 @@ ESC=$(printf '\033')
 printf "${ESC}[1;36m%s${ESC}[m\n" '***** php composer updating... *****'
 
 if ! command -v composer 1>/dev/null 2>&1; then
-    printf "${ESC}[1;31m%s${ESC}[m\n" '***** php compser NOT installed. *****'
+    printf "${ESC}[1;31m%s${ESC}[m\n" '***** php composer NOT installed. *****'
     exit 1
 fi
 
 (cd "${HOME}" &&
-    composer update &&
     composer global update &&
+    composer update &&
     printf "${ESC}[1;32m%s${ESC}[m\n" '***** php composer updated. *****' &&
     exit 0) ||
     (printf "${ESC}[1;31m%s${ESC}[m\n" '***** php composer update failed. *****' &&
